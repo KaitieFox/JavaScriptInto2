a55cd22879f74b0f0071c53249e0a594acc53814
KaitieFox <kaitiefox@gmail.com>
2017-11-07 13:41:57 -0500

    Added button function

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,47 @@
+﻿//console.log("Hello!");
+//var name = "Kait";
+
+//console.log("Hello " + name);
+
+//for (i = 0; i < 11; i++)
+//{
+//    console.log(i);
+//}
+
+//function helloWorld() {
+//    var greeting = "Hello world!";
+//    alert(greeting);
+//}
+//helloWorld();
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(9, 8));
+
+console.log(add(5, "cat"));
+
+
+//this is called an anonymous function
+var otherFunction = function (a, b) {
+    return a + b;
+}
+console.log(otherFunction(2, .4));
+
+//write a function that takes in a name and writes hello [name]
+
+function yourName(name) {
+    console.log("Hello, " + name);
+    //return "Hello " + name;
+}
+yourName("Kait");
+//console.log(yourName("Kait"));
+
+function updateDiv() {
+    var outputString = "Hello and welcome to my webpage.";
+    document.getElementById("sectionOne").innerHTML = "<p>" + outputString + "</p>";
+}
+
+function swap() {
+
+}
