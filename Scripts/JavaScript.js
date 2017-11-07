8360fa0e9988419820c1eb159bb0c509332c4f1a
KaitieFox <kaitiefox@gmail.com>
2017-11-07 14:51:00 -0500

    Done with Inclass

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -37,11 +37,36 @@ function yourName(name) {
 yourName("Kait");
 //console.log(yourName("Kait"));
 
-function updateDiv() {
+function updateDiv()
+{
     var outputString = "Hello and welcome to my webpage.";
     document.getElementById("sectionOne").innerHTML = "<p>" + outputString + "</p>";
 }
 
-function swap() {
+function swap()
+{
+    var imageSrc = document.getElementById("swapImage").src;
+    
+    if (imageSrc.includes("planet"))
+    {
+        document.getElementById("swapImage").src = "Images/time-2919087_1920.jpg";
+    }
+    else
+    {
+        document.getElementById("swapImage").src = "Images/planet-1702788_1920.jpg";
+    }
+    console.log(document.getElementById("swapImage").src);
+}
 
+function printArray()
+{
+    var favFoods = ["Pizza", "Pasta", "Tacos", "Burritos", "Purritos"];
+    var outputText = "<ul>";
+
+    for(i=0; i<favFoods.length; i++)
+    {
+        outputText += "<li>" + favFoods[i] + "</li>";
+    }
+    outputText += "</ul>";
+    document.getElementById("sectionTwo").innerHTML += outputText;
 }
